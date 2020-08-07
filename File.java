d8f61ad3c67f66a0be465f8361f7228465e297ca
codegurudummy <66222331+codegurudummy@users.noreply.github.com>
2020-08-06 22:38:56 -0400

    Create File.java

---
+++ File.java
@@ -0,0 +1,13 @@
+import com.amazonaws.services.kinesis.AmazonKinesisClient;
+import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+
+import example.foo.FooClient;
+
+public class DummyFile {
+    
+}
